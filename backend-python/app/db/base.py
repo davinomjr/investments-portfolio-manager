7861d107a68c790ea10beed5f31c8c7e27f918e6
Davino Junior <davinomaurojr@gmail.com>
2026-03-08 21:09:02 -0300

    Remove python backend

--- backend-python/app/db/base.py
+++
@@ -1,6 +0,0 @@
-from app.models.asset import Asset
-from app.models.asset_metadata import AssetMetadata
-from app.models.import_job import ImportJob
-from app.models.position import Position
-
-__all__ = ["Asset", "AssetMetadata", "ImportJob", "Position"]
